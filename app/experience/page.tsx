cb64b6727b72a4cf0c4fc657528bfe6835ae1323
Hanmin Kim <hanminkim123@gmail.com>
2024-04-14 09:02:42 -0400

    feat: add experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,6 +1,47 @@
+import Image, { StaticImageData } from "next/image";
+import RobloxLogo from "../../public/company_logos/roblox.jpg";
+import GraycoreLogo from "../../public/company_logos/graycore.jpg";
+
+function ExpNode({ src, title, company, duration, location, description }: {
+  src: StaticImageData,
+  title: string,
+  company: string,
+  duration: string,
+  location?: string,
+  description: string
+}) {
+  return <div className="flex flex-row space-x-4">
+    <div className="flex flex-col items-center">
+      <Image src={src} alt="company-logo" className="w-16 h-16 min-h-16 min-w-16 rounded-lg" />
+      <span className="h-full w-0.5 bg-gray-600" />
+    </div>
+    <div>
+      <p className="text-xl font-semibold">{title}</p>
+      {location ? <p className="italic text-sm">{company} @ {location}</p> :
+        <p className="italic text-sm">{company} (Remote)</p>}
+      <p className="italic text-sm">{duration}</p>
+      <p className="mt-2 mb-4 text-sm">{description}</p>
+    </div>
+  </div>;
+}
+
+function ExpList({ children }: { children: React.ReactNode }) {
+  return <div className="mt-4 space-y-2 w-full">{children}</div>;
+}
+
+
 export default function Home() {
   return <>
     <p className="text-xl font-bold">Experience</p>
     <p>Places I've worked at over the years.</p>
+
+    <ExpList>
+      <ExpNode src={RobloxLogo} title="Software Engineer Intern" company="Roblox" duration="May 2024 - August 2024"
+               location="San Mateo, CA"
+               description="I'll be joining Roblox this summer!" />
+      <ExpNode src={GraycoreLogo} title="Software Engineering Intern" company="Graycore"
+               duration="May 2023 - August 2023"
+               description="I did a variety of tasks, such as integrating Nx into our Angular monorepo CI pipeline, creating a custom systems integration to transform and update information between enterprise systems, and buitl a system to automatically track and synchronize data using a custom Hubspot public app and Azure Functions." />
+    </ExpList>
   </>;
 }
