a81ea7ae7129d91e72fa8da455559eef174084db
Hanmin Kim <hanminkim123@gmail.com>
2024-04-14 14:57:03 -0400

    feat: reduce header size if on narrow device

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import Logo from "../../public/logo512.png";
+import Image from "next/image";
 
 export function Header() {
   return <header className="navbar navbar-sticky navbar-glass">
     <div className="navbar-start">
       <Link className="navbar-item link" href="/">
-        <label className="btn btn-ghost flex cursor-pointer px-2 space-x-3" tabIndex={0}>
-          <img src="/logo192.png" alt="avatar" className="h-8" />
-          <p className="text-lg">Hanmin Kim</p>
+        <label className="btn btn-ghost flex cursor-pointer px-1 space-x-0 sm:space-x-3 sm:px-2" tabIndex={0}>
+          <Image src={Logo} alt="avatar" className="h-8 w-8 min-w-8" />
+          <p className="text-lg overflow-clip max-w-0 sm:max-w-fit">Hanmin Kim</p>
         </label>
       </Link>
     </div>
