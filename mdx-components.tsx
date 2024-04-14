54400482f555fce20bde23c13f9fec57914e2419
Hanmin Kim <hanminkim123@gmail.com>
2024-04-14 12:37:19 -0400

    fix: add mdx-components to tailwind config

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,9 +2,9 @@ import type { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 className="text-xl font-bold">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-lg font-bold">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-base font-bold">{children}</h3>,
+    h1: ({ children }) => <h1 className="text-3xl font-bold">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-2xl font-semibold">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-lg font-semibold">{children}</h3>,
     p: ({ children }) => <p className="text-content">{children}</p>,
     a: ({ children, href }) => <a className="text-blue-500" href={href}>{children}</a>,
     ...components,
