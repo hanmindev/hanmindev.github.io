99e860d8942338ad6cfdb39b1b117ffeb0b5da25
Hanmin Kim <hanminkim123@gmail.com>
2024-04-14 10:22:53 -0400

    feat: clean VideoPlayer

--- app/components/VideoPlayer.tsx
+++ app/components/VideoPlayer.tsx
@@ -1,17 +1,24 @@
-"use client"
+"use client";
 
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import useOnScreen from "../helpers/hooks";
+import Image from "next/image";
 
-function VideoPlayer({ raw, type, video_link, play, autoplay }: { raw?: boolean; type: string; video_link: string; play?: boolean; autoplay?: boolean }) {
+function VideoPlayer({ raw, type, video_link, play, autoplay }: {
+  raw?: boolean;
+  type: string;
+  video_link: string;
+  play?: boolean;
+  autoplay?: boolean
+}) {
   const video = useRef<HTMLVideoElement>(null);
 
-  const isVisible = useOnScreen(video)
+  const isVisible = useOnScreen(video);
 
-  if (video.current){
+  if (video.current) {
     if ((play || autoplay) && isVisible) {
       video.current.play();
-    }else{
+    } else {
       video.current.pause();
     }
   }
@@ -19,31 +26,31 @@ function VideoPlayer({ raw, type, video_link, play, autoplay }: { raw?: boolean;
   let inner;
 
   if (type === "iframe") {
-    if (raw){
-      video_link = `https://www.youtube.com/embed/${video_link}`
+    if (raw) {
+      video_link = `https://www.youtube.com/embed/${video_link}`;
     }
 
-    inner = <iframe title="Featured Video" width="100%" height="100%" src={video_link}/>
+    inner = <iframe title="Featured Video" className="w-full h-full" src={video_link} />;
 
   } else if (type === "gfycat") {
-    if (raw){
-      video_link = `https://thumbs.gfycat.com/${video_link}-mobile.mp4`
+    if (raw) {
+      video_link = `https://thumbs.gfycat.com/${video_link}-mobile.mp4`;
     }
-    inner = <video ref={video} autoPlay={autoplay} loop muted width="100%" height="100%" style={{margin: 0}}>
-        <source src={video_link} type="video/mp4" />
-      </video>
+    inner = <video ref={video} autoPlay={autoplay} loop muted className="w-full h-full m-0">
+      <source src={video_link} type="video/mp4" />
+    </video>;
 
   } else if (type === "image") {
-    inner = <img width="100%" height="100%" src={video_link} alt={video_link}/>
+    inner = <Image className="w-full h-full" src={video_link} alt={video_link} />;
   } else {
-    return <p>VideoPlayer Error</p>
+    return <p>VideoPlayer Error</p>;
   }
 
   return (
     <div className="rounded-md mb-3 overflow-clip">
       {inner}
     </div>
-  )
+  );
 }
 
 export default VideoPlayer;
