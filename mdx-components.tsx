65ef1a0850a8e4ed34947beccafc76a92b8aa019
Hanmin Kim <hanminkim123@gmail.com>
2024-04-15 01:39:09 -0400

    feat: update mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,17 @@
-import type { MDXComponents } from 'mdx/types'
+import type { MDXComponents } from "mdx/types";
+import React from "react";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 className="text-3xl font-bold">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-2xl font-semibold">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-lg font-semibold">{children}</h3>,
-    p: ({ children }) => <p className="text-content">{children}</p>,
-    a: ({ children, href }) => <a className="text-blue-500" href={href}>{children}</a>,
-    ...components,
-  }
+    h1: ({ children }) => <h1 className="text-4xl font-bold mt-4">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-3xl font-semibold mt-4">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-2xl font-semibold mt-4">{children}</h3>,
+    h4: ({ children }) => <h4 className="text-xl font-semibold mt-4">{children}</h4>,
+    h5: ({ children }) => <h5 className="text-lg font-semibold mt-4">{children}</h5>,
+    h6: ({ children }) => <h6 className="text-base font-semibold mt-4">{children}</h6>,
+    p: ({ children }) => <p className="text-base mt-2">{children}</p>,
+    a: ({ children, href }) => <a className="text-base text-blue-500 underline" href={href}>{children}</a>,
+    hr: () => <div className="divider"></div>,
+    ...components
+  };
 }
