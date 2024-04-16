6bd4193515a51e0e9924b197896e5365298f0bcd
Hanmin Kim <hanminkim123@gmail.com>
2024-04-15 21:31:06 -0400

    feat: change base path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withMDX = require("@next/mdx")();
 
 module.exports = withMDX({
   output: "export",
-  basePath: "/beta-website",
+  basePath: "",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     unoptimized: true
