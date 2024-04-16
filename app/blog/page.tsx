2684716062dfb8d55b6c89304d73093d31bfe2e7
Hanmin Kim <hanminkim123@gmail.com>
2024-04-15 22:35:53 -0400

    feat: merge blog and doc cards into one component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,26 +1,10 @@
-import Link from "next/link";
-
 import { BlogMeta } from "@/app/blog/*/page.mdx";
-
-function BlogCard({ title, description, link }: { title: string, description: string, link: string }) {
-  return <div className="group card max-w-full transition-transform duration-100 hover:scale-105">
-    <Link className="card-body" href={link}>
-      <h2 className="card-header group-hover:text-blue-500 dark:group-hover:text-blue-300">{title}</h2>
-      <p className="text-content2 group-hover:text-blue-500 dark:group-hover:text-blue-300">{description}</p>
-    </Link>
-  </div>;
-}
-
-function BlogList({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col space-y-4">
-    {children}
-  </div>;
-}
+import { Card, CardList } from "@/app/components/card_list";
 
 function BlogCardWrapper({ metadata, link }: { metadata: BlogMeta, link: string }) {
   const { title, description } = metadata;
 
-  return <BlogCard title={title} description={description} link={link} />;
+  return <Card title={title} description={description} link={link} />;
 }
 
 // I really don't like this, but I can't think of a better static way to do this
@@ -36,13 +20,13 @@ export default function Home() {
     <p className="text-xl font-bold">Blog</p>
     <p>Random writeups.</p>
     <div className="divider"></div>
-    <BlogList>
+    <CardList>
       <BlogCardWrapper metadata={uoft_cs} link="/blog/uoft-cs-guide" />
       <BlogCardWrapper metadata={mirrorboard} link="/blog/mirrorboard" />
       <BlogCardWrapper metadata={graphing} link="/blog/3d-graphing-minecraft" />
       <BlogCardWrapper metadata={mcmv} link="/blog/mcmv" />
       <BlogCardWrapper metadata={split_keyboard} link="/blog/split-keyboard" />
       <BlogCardWrapper metadata={btc} link="/blog/minecraft-bitcoin" />
-    </BlogList>
+    </CardList>
   </>;
 }
--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,30 +1,14 @@
-import Link from "next/link";
-
-function DocsCard({ title, description, link }: { title: string, description: string, link: string }) {
-  return <div className="group card max-w-full transition-transform duration-100 hover:scale-105">
-    <Link className="card-body" href={link}>
-      <h2 className="card-header group-hover:text-blue-500 dark:group-hover:text-blue-300">{title}</h2>
-      <p className="text-content2 group-hover:text-blue-500 dark:group-hover:text-blue-300">{description}</p>
-    </Link>
-  </div>;
-}
-
-function DocsList({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col space-y-4">
-    {children}
-  </div>;
-}
-
+import { Card, CardList } from "@/app/components/card_list";
 
 export default function Home() {
   return <>
     <p className="text-xl font-bold">Docs</p>
     <p>Some of my projects need some additional information, which I have compiled into docs.</p>
     <div className="divider"></div>
-    <DocsList>
-      <DocsCard title="Guide to the Ingot Programming Language"
+    <CardList>
+      <Card title="Guide to the Ingot Programming Language"
                 description="Ingot is a high-level programming language that compiles to mcfunction for Minecraft datapack development. Warning: This is under active development and is not yet stable."
                 link="https://hanmin.dev/ingot-docs/" />
-    </DocsList>
+    </CardList>
   </>;
 }
