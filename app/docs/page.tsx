59f023ff4076f50101702c09298b61519afaea85
Hanmin Kim <hanminkim123@gmail.com>
2024-04-15 22:32:07 -0400

    fix: update docs card too

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 function DocsCard({ title, description, link }: { title: string, description: string, link: string }) {
   return <div className="group card max-w-full transition-transform duration-100 hover:scale-105">
     <Link className="card-body" href={link}>
-      <h2 className="card-header group-hover:text-blue-300">{title}</h2>
-      <p className="text-content2 group-hover:text-blue-300">{description}</p>
+      <h2 className="card-header group-hover:text-blue-500 dark:group-hover:text-blue-300">{title}</h2>
+      <p className="text-content2 group-hover:text-blue-500 dark:group-hover:text-blue-300">{description}</p>
     </Link>
   </div>;
 }
