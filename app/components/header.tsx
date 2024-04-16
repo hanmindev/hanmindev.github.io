091bddbdd094fc95444c5fa16686500e0efb6b13
Hanmin Kim <hanminkim123@gmail.com>
2024-04-15 21:30:07 -0400

    feat: make site mobile-friendly

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,16 +8,16 @@ export function Header() {
       <Link className="navbar-item link" href="/">
         <label className="btn btn-ghost flex cursor-pointer px-1 space-x-0 sm:space-x-3 sm:px-2" tabIndex={0}>
           <Image src={Logo} alt="avatar" className="h-8 w-8 min-w-8" />
-          <p className="text-lg overflow-clip max-w-0 sm:max-w-fit">Hanmin Kim</p>
+          <p className="text-lg overflow-clip hidden sm:inline">Hanmin Kim</p>
         </label>
       </Link>
     </div>
-    <div className="navbar-end">
-      <Link className="navbar-item" href="/">Home</Link>
-      <Link className="navbar-item" href="/experience">Experience</Link>
-      <Link className="navbar-item" href="/projects">Projects</Link>
-      <Link className="navbar-item" href="/docs">Docs</Link>
-      <Link className="navbar-item" href="/blog">Blog</Link>
+    <div className="navbar-end text-sm sm:text-base space-x-0">
+      <Link className="navbar-item p-1 sm:p-2" href="/">Home</Link>
+      <Link className="navbar-item p-1 sm:p-2" href="/experience">Experience</Link>
+      <Link className="navbar-item p-1 sm:p-2" href="/projects">Projects</Link>
+      <Link className="navbar-item p-1 sm:p-2" href="/docs">Docs</Link>
+      <Link className="navbar-item p-1 sm:p-2" href="/blog">Blog</Link>
     </div>
   </header>;
 }
