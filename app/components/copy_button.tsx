a669737f0118f08f29f22d81d5b72400178b8ce9
Hanmin Kim <hanminkim123@gmail.com>
2024-04-18 17:16:47 -0400

    feat(blog): add copy button to sections

---
+++ app/components/copy_button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+
+export function CopyButton({ className, children, copy_value, prepend_link, enter_link }: {
+  className?: string,
+  children: React.ReactNode,
+  copy_value: string,
+  prepend_link?: boolean
+  enter_link?: boolean
+}) {
+  const copy_fn = () => {
+    if (prepend_link) copy_value = window.location.origin + window.location.pathname + copy_value;
+    navigator.clipboard.writeText(copy_value);
+  };
+
+
+  if (enter_link) {
+    return <Link href={copy_value} className={className} onClick={copy_fn}>
+      {children}
+    </Link>;
+  }
+
+  return <div className={className} onClick={copy_fn}>
+    {children}
+  </div>;
+}
