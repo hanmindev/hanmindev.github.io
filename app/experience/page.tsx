443e03c9e9eea2d831dcd18a8bbf501cb6051d73
Hanmin Kim <hanminkim123@gmail.com>
2024-04-25 01:28:37 -0400

    feat(exp): reword

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -40,7 +40,7 @@ function ExpList({ children }: { children: React.ReactNode }) {
 export default function Home() {
   return <>
     <p className="text-xl font-bold">Experience</p>
-    <p>Places I've worked at over the years.</p>
+    <p>Places I've been over the years.</p>
 
     <ExpList>
       <ExpNode src={RobloxLogo} title="Software Engineer Intern" company="Roblox" duration="May 2024 - August 2024"
@@ -48,10 +48,10 @@ export default function Home() {
                description="I'll be joining Roblox this summer!" />
       <ExpNode src={GraycoreLogo} title="Software Engineering Intern" company="Graycore"
                duration="May 2023 - August 2023"
-               description="I did a variety of tasks, such as integrating Nx into our Angular monorepo CI pipeline, creating a custom systems integration to transform and update information between enterprise systems, and built a system to automatically track and synchronize data using a custom Hubspot public app and Azure Functions." />
+               description="I did a variety of tasks, such as integrating Nx into our Angular monorepo CI pipeline, creating a custom systems integration to transform and update information between enterprise systems, and building a system to automatically track and synchronize data using a custom Hubspot public app and Azure Functions." />
       <ExpNode src={NorvaleLogo} title="Software Developer" company="Norvale"
                duration="August 2018 - December 2022"
-               description="I worked with Norvale for many years (even longer if you consider the projects I worked on with them before they were a company!). I was the main developer for several projects where I programmed the game logic, UI, physics engines, and more, which would make their way into Microsoft's Minecraft Marketplace." />
+               description="I worked with Norvale for many years (even longer if you consider the projects I worked on with them before they were a company!). I was the main developer for several projects where I programmed the game logic, UI, physics engines, and more, which made their way into Microsoft's Minecraft Marketplace." />
     </ExpList>
   </>;
 }
