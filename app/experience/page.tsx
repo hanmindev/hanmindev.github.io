25d58a6c55bc8a9752b24113bac1e8a93451a1eb
Hanmin Kim <hanminkim123@gmail.com>
2024-06-20 20:18:13 -0700

    feat(experience): update experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
                description="To improve the consistency of our Cloud Security baseline, I am currently building an end-to-end system to monitor 10k+ security control instances throughout the organization using Go and AWS. Aspects that my system will monitor include the health status of deployment pipelines and infrastructure drift." />
       <ExpNode src={GraycoreLogo} title="Software Engineering Intern" company="Graycore"
                duration="May 2023 - August 2023"
-               description="I did a variety of tasks, such as integrating Nx into our Angular monorepo CI pipeline, creating a custom systems integration to transform and update information between enterprise systems, and building a system to automatically track and synchronize data using a custom Hubspot public app and Azure Functions." />
+               description="Performed a variety of tasks, such as integrating Nx into our Angular monorepo CI pipeline, creating a custom systems integration to transform and update information between enterprise systems, and building a system to automatically track and synchronize data using a custom Hubspot public app and Azure Functions." />
       <ExpNode src={NorvaleLogo} title="Software Developer" company="Norvale"
                duration="August 2018 - December 2022"
                description="I worked with Norvale for many years (even longer if you consider the projects I worked on with them before they were a company!). I was the main developer for several projects where I programmed the game logic, UI, physics engines, and more, which made their way into Microsoft's Minecraft Marketplace." />
